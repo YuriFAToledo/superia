0e6abd8eb769668649bc5c95a650e2b146017145
YuriFAToledo <yuri.toledo@sou.inteli.edu.br>
2025-10-01 15:37:22 -0300

    fix: rota de reprocessamento

--- src/features/notas/hooks/useNotasFiscais.ts
+++ src/features/notas/hooks/useNotasFiscais.ts
@@ -337,7 +337,7 @@ export function useNotasFiscais(initialParams: NotasParams = {}) {
                 'Content-Type': 'application/json'
             };
             
-            await axios.patch(`${API_URL}/${nota.numero_nf}/retry`, 
+            await axios.patch(`https://superia-trading.app.n8n.cloud/webhook/2549b8a5-a9e0-4855-a8c1-cbe6b9a2db4e/nfs-pendentes/${nota.numero_nf}/retry`, 
                 { motivo },
                 { headers }
                 //:numero_nf/retry
